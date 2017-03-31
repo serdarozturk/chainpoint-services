91de19e95ec6600f42e733b6cb355058f4840fef
Glenn Rempe <glenn@tierion.com>
2017-03-31 10:33:54 -0700

    Update aggregator to run periodically. Use DotEnv to specify interval w/ 1000ms default.

--- node-aggregator-service/README.md
+++ node-aggregator-service/README.md
@@ -14,20 +14,6 @@ Run Container Locally
 docker run -p 49160:8080 -d chainpoint/node-aggregator-service
 ```
 
-Open in Browser or Curl
-
-```
-curl http://127.0.0.1:49160/
-
-open http://127.0.0.1:49160/
-```
-
-Micro Load Test w/ Apache Bench
-
-```
-ab -n 1000 -c 25 http://127.0.0.1:49160/
-```
-
 Login To Local Container
 
 ```
--- node-aggregator-service/server.js
+++ node-aggregator-service/server.js
@@ -1,15 +1,17 @@
-'use strict'
+require('dotenv').config()
 
-const express = require('express')
+// Using DotEnv : https://github.com/motdotla/dotenv
+// Expects AGGREGATION_INTERVAL environment variable
+// Defaults to 1000ms if not present. Can set vars in
+// `.env` file (do NOT commit to repo) or on command
+// line:
+//   AGGREGATION_INTERVAL=200 node server.js
+//
+const INTERVAL = process.env.AGGREGATION_INTERVAL || 1000
 
-// Constants
-const PORT = 8080
+let aggregate = function () {
+  console.log('merkling every %sms ...', INTERVAL)
+}
 
-// App
-const app = express()
-app.get('/', function (req, res) {
-  res.send('Aggregate all the things!\n')
-})
-
-app.listen(PORT)
-console.log('Running on http://localhost:' + PORT)
+// aggregate periodically
+let timerId = setInterval(() => aggregate(), INTERVAL)
