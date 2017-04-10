510b261e2d4f520c11c491c19268dd640d91d90d
Jason Bukowski <jasonbukowski@users.noreply.github.com>
2017-04-10 11:38:12 -0400

    Added splitter service functionality

--- node-splitter-service/README.md
+++ node-splitter-service/README.md
@@ -21,3 +21,58 @@ docker ps
 docker exec -it <container id> /bin/ash
 ```
 
+## Configuration
+Configuration parameters will be stored in environment variables. Environment variables can be overridden throught the use of a .env file. 
+
+The following are the descriptions of the configuration parameters:
+
+| Name           | Description  |
+| :------------- |:-------------|
+| RMQ\_WORK\_EXCHANGE\_NAME       | the name of the RabbitMQ topic exchange to use 
+| RMQ\_WORK\_IN\_ROUTING\_KEY     | the topic exchange routing key for message consumption originating the web service
+| RMQ\_WORK\_OUT\_ROUTING\_KEY       | the topic exchange routing key for message publishing bound for the proof state service 
+| RABBITMQ\_CONNECT\_URI       | the RabbitMQ connection URI 
+
+The following are the types, defaults, and acceptable ranges of the configuration parameters: 
+
+| Name           | Type         | Default | 
+| :------------- |:-------------|:-------------|
+| RMQ\_WORK\_EXCHANGE\_NAME       | string       | 'work\_topic\_exchange' | 
+| RMQ\_WORK\_IN\_ROUTING\_KEY     | string       | 'work.splitter' | 
+| RMQ\_WORK\_OUT\_ROUTING\_KEY       | string       | 'work.splitter.state' |  
+| RABBITMQ\_CONNECT\_URI       | string      | 'amqp://chainpoint:chainpoint@rabbitmq' | 
+
+
+
+## Splitting Process
+The service will receive persistent hash object messages via a subscription to a durable queue bound to a durable direct exchange within RabbitMQ. The routing key to bind to is defined by the RMQ\_WORK\_IN\_ROUTING\_KEY configuration parameter.
+
+The following is an example of a hash object array message body: 
+```json
+[
+  {
+    "id": "34712680-14bb-11e7-9598-0800200c9a66",
+    "hash": "39ec487d38b828c6f9ef5d1e8128c76c7455d0f5cbf7ce9b8ef550cf223dfbc3"
+  },
+  {
+    "id": "6d627180-1883-11e7-a8f9-edb8c212ef23",
+    "hash": "ed10960ccc613e4ad0533a813e2027924afd051f5065bb5379a80337c69afcb4"
+  }
+]
+```
+| Name | Description                                                            |
+| :--- |:-----------------------------------------------------------------------|
+| id   | The UUIDv1 unique identifier for a hash object with embedded timestamp |
+| hash | A hex string representing the hash to be processed                     |
+
+Once a message is consumed, the hash object array is split into individual hash objects. Each individual hash object is published using the RMQ\_WORK\_OUT\_ROUTING\_KEY for consumption by the proof state service. When all hash objects have been published, the original message is acked.
+
+## Service Failure
+In the event of any error occurring, the service will log that error to STDERR and kill itself with a process.exit(). RabbitMQ will be configured so that upon service exit, unacknowledged messages will be requeued to ensure than unfinished work lost due to failure will be processed again in full.
+
+
+## Notable NPM packages
+| Name         | Description                                                            |
+| :---         |:-----------------------------------------------------------------------|
+| dotenv       | for managing and optionally overriding environment variables |
+| amqplib      | for communication between the service and RabbitMQ |
--- node-splitter-service/server.js
+++ node-splitter-service/server.js
@@ -1,9 +1,79 @@
-const _ = require('lodash')
+const amqp = require('amqplib')
+
 require('dotenv').config()
 
-// just a temp function
-let split = function () {
-  console.log('Splitting...')
+// The RabbitMQ exchange name
+const RMQ_WORK_EXCHANGE_NAME = process.env.RMQ_WORK_EXCHANGE_NAME || 'work_topic_exchange'
+
+// The RabbitMQ topic key for incoming message from the web service
+const RMQ_WORK_IN_ROUTING_KEY = process.env.RMQ_WORK_IN_ROUTING_KEY || 'work.splitter'
+
+// The RabbitMQ topic key for outgoing message to the proof state service
+const RMQ_WORK_OUT_ROUTING_KEY = process.env.RMQ_WORK_OUT_ROUTING_KEY || 'work.splitter.state'
+
+// Connection string w/ credentials for RabbitMQ
+const RABBITMQ_CONNECT_URI = process.env.RABBITMQ_CONNECT_URI || 'amqp://chainpoint:chainpoint@rabbitmq'
+
+// The channel used for all amqp communication
+// This value is set once the connection has been established
+var amqpChannel = null
+
+/**
+ * Opens an AMPQ connection and channel
+ * Retry logic is included to handle losses of connection
+ *
+ * @param {string} connectionString - The connection string for the RabbitMQ instance, an AMQP URI
+ */
+function amqpOpenConnection (connectionString) {
+  amqp.connect(connectionString).then(function (conn) {
+    conn.on('close', () => {
+      // if the channel closes for any reason, attempt to reconnect
+      console.error('Connection to RMQ closed.  Reconnecting in 5 seconds...')
+      amqpChannel = null
+      setTimeout(amqpOpenConnection.bind(null, connectionString), 5 * 1000)
+    })
+    conn.createConfirmChannel().then(function (chan) {
+      // the connection and channel have been established
+      // set 'amqpChannel' so that publishers have access to the channel
+      console.log('Connection established')
+      chan.assertExchange(RMQ_WORK_EXCHANGE_NAME, 'topic', { durable: true })
+      amqpChannel = chan
+
+      // Continuously load the HASHES from RMQ with hash objects to process
+      return amqpChannel.assertQueue('', { durable: true }).then(function (q) {
+        amqpChannel.bindQueue(q.queue, RMQ_WORK_EXCHANGE_NAME, RMQ_WORK_IN_ROUTING_KEY)
+        return amqpChannel.consume(q.queue, function (msg) {
+          if (msg !== null) {
+            let incomingHashBatch = JSON.parse(msg.content.toString()).hashes
+
+            // process each hash from the batch of hashes as submitted to the API
+            var hashPromises = incomingHashBatch.map(function (hashObj) {
+              console.log(hashObj)
+              return amqpChannel.publish(RMQ_WORK_EXCHANGE_NAME, RMQ_WORK_OUT_ROUTING_KEY, new Buffer(JSON.stringify(hashObj)), { persistent: true },
+              function (err, ok) {
+                if (err !== null) {
+                  console.error(RMQ_WORK_OUT_ROUTING_KEY, 'publish message nacked')
+                } else {
+                  console.log(RMQ_WORK_OUT_ROUTING_KEY, 'publish message acked')
+                }
+              })
+            })
+
+            Promise.all(hashPromises)
+            .then(() => {
+              amqpChannel.ack(msg)
+              console.log(RMQ_WORK_IN_ROUTING_KEY, 'consume message acked')
+            })
+          }
+        })
+      })
+    })
+  }).catch(() => {
+    // catch errors when attempting to establish connection
+    console.error('Cannot establish connection. Attempting in 5 seconds...')
+    setTimeout(amqpOpenConnection.bind(null, connectionString), 5 * 1000)
+  })
 }
 
-setInterval(() => split(), 1000)
+// Start reading from queue and splitting hashes
+amqpOpenConnection(RABBITMQ_CONNECT_URI)
