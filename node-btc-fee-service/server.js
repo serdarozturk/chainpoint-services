224ecc3366fbaa2fba6de07d643c99fb96c2bd5d
Glenn Rempe <glenn@tierion.com>
2017-04-18 22:40:28 -0700

    Enhance BTC fee service to provide real time BTC pricing for TX
    Provides an enhanced data structure in RMQ and Redis which contains:
    
    {
      "recFeeInSatPerByte":160,
      "recFeeInSatForAvgTx":37600,
      "recFeeInBtcForAvgTx":0.000376,
      "recFeeInUsdForAvgTx":0.48,
      "avgTxSizeBytes":235
    }
    
    This provides some data about what the actual USD cost for publishing
    a TX will be based on the current cost measured in satoshis/byte multiplied
    by the average tx size and converts it to btc/tx and usd/tx.
    
    There is a primary and a fallback provider of exchange data. If both
    are unavailable the services continues to work, just drops the USD
    conversion data and returns null for that value instead.

--- node-btc-fee-service/server.js
+++ node-btc-fee-service/server.js
@@ -1,6 +1,8 @@
 const amqp = require('amqplib')
 const async = require('async')
 const _ = require('lodash')
+const sb = require('satoshi-bitcoin')
+const coinTicker = require('coin-ticker')
 require('dotenv').config()
 
 const REDIS_CONNECT_URI = process.env.REDIS_CONNECT_URI || 'redis://redis:6379'
@@ -14,10 +16,10 @@ const coreCacheTransporters = requestify.coreCacheTransporters
 requestify.cacheTransporter(coreCacheTransporters.redis(redis))
 
 // The name of the RabbitMQ topic exchange to use
-const RMQ_WORK_EXCHANGE_NAME = process.env.RMQ_WORK_EXCHANGE_NAME || 'work_topic_exchange'
+const RMQ_EXCHANGE_NAME = process.env.RMQ_EXCHANGE_NAME || 'work_topic_exchange'
 
 // The topic exchange routing key for message publishing bound for the BTC transaction service
-const RMQ_WORK_OUT_ROUTING_KEY = process.env.RMQ_WORK_OUT_ROUTING_KEY || 'work.btc.rec_tx_fee'
+const RMQ_ROUTING_KEY = process.env.RMQ_ROUTING_KEY || 'btc.rec_fee'
 
 // Connection string w/ credentials for RabbitMQ
 const RABBITMQ_CONNECT_URI = process.env.RABBITMQ_CONNECT_URI || 'amqp://chainpoint:chainpoint@rabbitmq'
@@ -27,18 +29,58 @@ const REC_FEES_URI = process.env.REC_FEES_URI || 'https://bitcoinfees.21.co/api/
 
 // The published key location where recommended fee will be stored
 // for use by other services.
-const BTC_REC_TX_FEE_KEY = process.env.BTC_REC_TX_FEE_KEY || 'btc_rec_tx_fee'
-
-// How frequently should this process tick (in milliseconds)
-const TICK_FREQ = 1000 * 5
+const BTC_REC_FEE_KEY = process.env.BTC_REC_FEE_KEY || 'btc_rec_fee'
 
 // How long, in milliseconds, should cached values be kept for
 // until a new HTTP GET is issued?
 const CACHE_TTL = 1000 * 60 * 10 // in ms, cache response for 10 min
 
+// The average size, in Bytes, for BTC transactions for anchoring
+const AVG_TX_BYTES = 235
+
 // The channel used for all amqp communication
 // This value is set once the connection has been established
 let amqpChannel = null
+
+// Periodically updated with most current data from Bitcoin Exchange
+let currentExchange = null
+
+let getCurrentExchange = function () {
+  coinTicker('bitfinex', 'btcusd').then(data => {
+    // console.log('bitfinex exchange data : %s', JSON.stringify(data))
+    currentExchange = data
+  }).catch(err => {
+    console.error('bitfinex call failed, failing over to bitstamp : %s', JSON.stringify(err))
+    coinTicker('bitstamp', 'btcusd').then(data => {
+      // console.log('bitstamp exchange data : %s', JSON.stringify(data))
+      currentExchange = data
+    }).catch(err => {
+      // Could not reach two exchanges. Something is very wrong.
+      console.error('bitstamp call failed : %s', JSON.stringify(err))
+    })
+  })
+}
+
+let genFeeRecObj = function (recFeeInSatoshiPerByte) {
+  let feeSatForAvgTx = Math.ceil(AVG_TX_BYTES * recFeeInSatoshiPerByte)
+  let feeBtcForAvgTx = sb.toBitcoin(feeSatForAvgTx)
+
+  let obj = {}
+  obj.recFeeInSatPerByte = recFeeInSatoshiPerByte
+  obj.recFeeInSatForAvgTx = feeSatForAvgTx
+  obj.recFeeInBtcForAvgTx = feeBtcForAvgTx
+
+  // Include USD estimation if available
+  if (_.has(currentExchange, 'last')) {
+    let fee = _.parseInt(currentExchange.last) * feeBtcForAvgTx
+    obj.recFeeInUsdForAvgTx = _.round(fee, 2)
+  } else {
+    obj.recFeeInUsdForAvgTx = null
+  }
+
+  obj.avgTxSizeBytes = AVG_TX_BYTES
+  return obj
+}
 
 // HTTP GET and cache current fees from
 let getRecommendedFees = function () {
@@ -55,19 +97,21 @@ let getRecommendedFees = function () {
     // Don't proceed, and exit to trigger investigation, if what we got back
     // from the API is not the expected structure or not a number value. It is
     // better for services dependent on that value to keep using the last stored
-    // value than to remove/overright with a bad value.
+    // value than to remove/overwrite with a bad value.
     if (_.has(responseBody, 'fastestFee') && _.isNumber(responseBody.fastestFee)) {
       let selectedFee = _.parseInt(responseBody.fastestFee)
-      console.log('bitcoinfees.21.co recommends %d satoshis/byte', selectedFee)
+      let feeRecObj = genFeeRecObj(selectedFee)
 
-      // Publish the recommended BTC Transaction fee in a well known location
-      // for use by any service with access to Redis. Value is measured
-      // in satoshis per byte that will result in desired response time.
-      redis.set(BTC_REC_TX_FEE_KEY, selectedFee)
+      console.log('fee recommendations %s', JSON.stringify(feeRecObj))
 
-      // Publish the same value onto RMQ route that can be consumed by interested services
-      let msg = new Buffer(JSON.stringify(selectedFee))
-      amqpChannel.publish(RMQ_WORK_EXCHANGE_NAME, RMQ_WORK_OUT_ROUTING_KEY, msg, function (err, ok) {
+      // Publish the recommended transaction fee data in a well known
+      // location for use by any service with access to Redis.
+      redis.set(BTC_REC_FEE_KEY, JSON.stringify(feeRecObj))
+
+      // Also publish the recommended transaction fee data onto an RMQ
+      // route that can be consumed by any interested services.
+      let msg = new Buffer(JSON.stringify(feeRecObj))
+      amqpChannel.publish(RMQ_EXCHANGE_NAME, RMQ_ROUTING_KEY, msg, function (err, ok) {
         if (err !== null) {
           console.error('RMQ publish failed : %s', JSON.stringify(err))
           process.exit(1)
@@ -105,7 +149,7 @@ function amqpOpenConnection (connectionString) {
       // the connection and channel have been established
       // set 'amqpChannel' so that publishers have access to the channel
       console.log('RMQ connection established')
-      chan.assertExchange(RMQ_WORK_EXCHANGE_NAME, 'topic', { durable: true })
+      chan.assertExchange(RMQ_EXCHANGE_NAME, 'topic', { durable: true })
       amqpChannel = chan
     })
   }).catch(() => {
@@ -118,5 +162,11 @@ function amqpOpenConnection (connectionString) {
 // AMQP initialization
 amqpOpenConnection(RABBITMQ_CONNECT_URI)
 
-// check for update frequently, most will retrieve from cache
-setInterval(() => getRecommendedFees(), TICK_FREQ)
+// get exchange rate at startup and periodically
+getCurrentExchange()
+setInterval(() => getCurrentExchange(), 1000 * 60 * 10) // 10 min
+
+// get recommended fees at startup and periodically
+// most will retrieve from cache and not hit external API
+getRecommendedFees()
+setInterval(() => getRecommendedFees(), 1000 * 5) // 5 seconds
