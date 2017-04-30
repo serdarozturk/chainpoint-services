ad3af671e6eb7eb2d8ed89b892b378704db8935b
Glenn Rempe <glenn@tierion.com>
2017-04-30 12:42:35 -0700

    JS Standard format and Prettify

--- node-btc-tx-service/server.js
+++ node-btc-tx-service/server.js
@@ -3,71 +3,62 @@ const _ = require('lodash')
 const bcoin = require('bcoin')
 require('dotenv').config()
 
-
-var Peer = bcoin.net.Peer;
-var NetAddress = bcoin.primitives.NetAddress;
-var Network = bcoin.protocol.Network;
-
+var Peer = bcoin.net.Peer
+var NetAddress = bcoin.primitives.NetAddress
+var Network = bcoin.protocol.Network
 
 let broadcastTx = (tx) => {
+  var network = Network.get('testnet')
 
-
-  var network = Network.get('testnet');
-
-  var peer, addr;
+  var peer, addr
 
   peer = Peer.fromOptions({
     network: 'testnet',
     agent: 'my-subversion',
     hasWitness: function () {
-      return false;
+      return false
     }
-  });
+  })
 
-  addr = NetAddress.fromHostname(process.env.BROADCAST_PEER, 'testnet');
-
+  addr = NetAddress.fromHostname(process.env.BROADCAST_PEER, 'testnet')
 
   return new Promise((resolve, reject) => {
-
-    peer.connect(addr);
-    peer.tryOpen();
+    peer.connect(addr)
+    peer.tryOpen()
 
     peer.on('error', function (err) {
-      reject(err);
-    });
-
+      reject(err)
+    })
 
     peer.on('open', function () {
-      peer.announceTX([tx]);
-      resolve(tx);
-    });
+      peer.announceTX([tx])
+      resolve(tx)
+    })
   })
 }
 
-let genTx = (merkleRoot, spendable_ouputs, master, change_path, txfee ) =>{
+let genTx = (merkleRoot, spendable_ouputs, master, change_path, txfee) => {
+  var keyring = bcoin.keyring()
+  var coins = []
 
-var keyring = bcoin.keyring()
-var coins = [];
+  for (var output in spendable_ouputs) {
+    keyring.add(master.derive(output.path))
+    var coin = bcoin.coin.fromTX(output.tx, output.idx, output.height)
+    coins.push(coin)
+  }
 
-for (var output in spendable_ouputs) {
-  keyring.add(master.derive(output.path))
-  var coin = bcoin.coin.fromTX(output.tx, output.idx, output.height);
-  coins.push(coin);
-}
+  var mtx = new bcoin.mtx()
 
-var mtx = new bcoin.mtx();
+  mtx.addOutput(bcoin.Script.fromNullData(merkle_root), null)
 
-
-mtx.addOutput(bcoin.Script.fromNullData(merkle_root),null);
-
-return mtx.fund(coins, {
-  rate:TXFEE,
-  changeAddress: master.derive(change_path).toAddress()
+  return mtx.fund(coins, {
+    rate: TXFEE,
+    changeAddress: master.derive(change_path).toAddress()
   }).then(
-    ()=>{
-      mtx.sign(keyring);
-      assert(mtx.verify());
-      var tx = mtx.toTX();
+    () => {
+      mtx.sign(keyring)
+      assert(mtx.verify())
+      var tx = mtx.toTX()
       return tx
     }
   )
@@ -75,34 +66,28 @@ return mtx.fund(coins, {
 
 // just a temp function
 let finalize = () => {
-
   console.log('BTC TX...')
 
-//Fetch this from Rabbit MQ?
-const merkle_root = Buffer();
+  // Fetch this from Rabbit MQ?
+  const merkle_root = Buffer()
 
+  const master = bcoin.hd.fromSeed(process.env.SEED)
 
-const master = bcoin.hd.fromSeed(process.env.SEED);
+  const TXFEE = parseInt(process.evn.TXFEE)
+  const TXAMOUNT = parseInt(process.evn.TXFEE)
 
-const TXFEE = parseInt(process.evn.TXFEE);
-const TXAMOUNT = parseInt(process.evn.TXFEE);
-
-//TODO get UTXO from somewhere
-//UTXO should store a tx, spendable indexes, path value, a key id, and a blockheight
-genTx(merkleRoot,spendable,master,changepath,TXFEE ).then((tx)=>{
-    return broadcastTx(tx); //TODO
-  }).then(()=>{
-    return pushTxIntoFullNodeMonitoring(tx); //TODO
+  // TODO get UTXO from somewhere
+  // UTXO should store a tx, spendable indexes, path value, a key id, and a blockheight
+  genTx(merkleRoot, spendable, master, changepath, TXFEE).then((tx) => {
+    return broadcastTx(tx) // TODO
+  }).then(() => {
+    return pushTxIntoFullNodeMonitoring(tx) // TODO
   })
-
-
-
-
 }
 
 setInterval(() => finalize(), 1000)
 
-//Export for unit tests
-module.exports={
-  genTx:genTx
+// Export for unit tests
+module.exports = {
+  genTx: genTx
 }
