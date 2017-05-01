e2bc75bdff5dcfbb3c0defc840fcc68ea0ba7b7d
Glenn Rempe <glenn@tierion.com>
2017-05-01 12:13:10 -0700

    WIP : Handoff to Jason

--- node-btc-tx-service/server.js
+++ node-btc-tx-service/server.js
@@ -3,24 +3,29 @@ const _ = require('lodash')
 const bcoin = require('bcoin')
 require('dotenv').config()
 
+// FIXME : point the default at production host later
+const BROADCAST_PEER_HOST = process.env.BROADCAST_PEER || 'bcoin.staging.chainpoint.org:18333'
+// FIXME : point the network type to 'main' later
+const BROADCAST_PEER_NET_TYPE = process.env.BROADCAST_PEER_NET_TYPE || 'testnet'
+
+const REDIS_CONNECT_URI = process.env.REDIS_CONNECT_URI || 'redis://redis:6379'
+
 var Peer = bcoin.net.Peer
 var NetAddress = bcoin.primitives.NetAddress
 var Network = bcoin.protocol.Network
 
 let broadcastTx = (tx) => {
-  var network = Network.get('testnet')
-
   var peer, addr
 
   peer = Peer.fromOptions({
-    network: 'testnet',
+    network: BROADCAST_PEER_NET_TYPE,
     agent: 'my-subversion',
     hasWitness: function () {
       return false
     }
   })
 
-  addr = NetAddress.fromHostname(process.env.BROADCAST_PEER, 'testnet')
+  addr = NetAddress.fromHostname(BROADCAST_PEER_HOST, BROADCAST_PEER_NET_TYPE)
 
   return new Promise((resolve, reject) => {
     peer.connect(addr)
@@ -64,12 +69,11 @@ let genTx = (merkleRoot, spendable_ouputs, master, change_path, txfee) => {
   )
 }
 
-// just a temp function
-let finalize = () => {
+let sendTxToBTC = () => {
   console.log('BTC TX...')
 
-  // Fetch this from Rabbit MQ?
-  const merkle_root = Buffer()
+  // FIXME : This will be injected into this service from RMQ. Hardcoded for initial test
+  const merkleRoot = Buffer.from('1c8a5401bcfa516bc481728692b7498f4d379c9404a2b3ff98cd78de01ad6f0f')
 
   const master = bcoin.hd.fromSeed(process.env.SEED)
 
@@ -85,7 +89,7 @@ let finalize = () => {
   })
 }
 
-setInterval(() => finalize(), 1000)
+setInterval(() => sendTxToBTC(), 1000 * 60 * 10) // 10 min
 
 // Export for unit tests
 module.exports = {
