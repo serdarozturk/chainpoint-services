eeb6c513e6ca9e2b4cb17fe5a42429649b958c4a
Glenn Rempe <glenn@tierion.com>
2017-05-15 18:16:45 -0700

    Add InfluxDB server suite and integrate btc-fee service

--- node-btc-fee-service/server.js
+++ node-btc-fee-service/server.js
@@ -1,7 +1,12 @@
 const _ = require('lodash')
 const sb = require('satoshi-bitcoin')
 const coinTicker = require('coin-ticker')
+const Influx = require('influx')
 require('dotenv').config()
+
+const INFLUXDB_HOST = process.env.INFLUXDB_HOST || 'influxdb'
+const INFLUXDB_PORT = process.env.INFLUXDB_PORT || 8086
+const INFLUXDB_DB = process.env.INFLUXDB_DB || 'chainpoint_fees'
 
 const REDIS_CONNECT_URI = process.env.REDIS_CONNECT_URI || 'redis://redis:6379'
 const r = require('redis')
@@ -27,18 +32,100 @@ const CACHE_TTL = 1000 * 60 * 10 // in ms, cache response for 10 min
 // The average size, in Bytes, for BTC transactions for anchoring
 const AVG_TX_BYTES = 235
 
-// Periodically updated with most current data from Bitcoin Exchange
-let currentExchange = null
+// Most current tick from current coin-ticker BTC Exchange
+let exchangeTick = null
 
-let getCurrentExchange = () => {
-  coinTicker('bitfinex', 'btcusd').then((data) => {
-    // console.log('bitfinex exchange data : %s', JSON.stringify(data))
-    currentExchange = data
+const influx = new Influx.InfluxDB({
+  host: INFLUXDB_HOST,
+  database: INFLUXDB_DB,
+  port: INFLUXDB_PORT,
+  schema: [
+    {
+      measurement: 'btc_fee_tick',
+      fields: {
+        last: Influx.FieldType.FLOAT,
+        ask: Influx.FieldType.FLOAT,
+        bid: Influx.FieldType.FLOAT,
+        low: Influx.FieldType.FLOAT,
+        high: Influx.FieldType.FLOAT,
+        vol: Influx.FieldType.FLOAT,
+        timestamp: Influx.FieldType.FLOAT,
+        exchange: Influx.FieldType.STRING,
+        pair: Influx.FieldType.STRING
+      },
+      tags: ['exchange']
+    },
+    {
+      measurement: 'btc_fee_rec',
+      fields: {
+        recFeeInSatPerByte: Influx.FieldType.INTEGER,
+        recFeeInSatForAvgTx: Influx.FieldType.INTEGER,
+        recFeeInBtcForAvgTx: Influx.FieldType.FLOAT,
+        recFeeInUsdForAvgTx: Influx.FieldType.FLOAT,
+        avgTxSizeBytes: Influx.FieldType.INTEGER
+      },
+      tags: []
+    }
+  ]
+})
+
+// Create InfluxDB database lazily
+influx.getDatabaseNames().then(names => {
+  if (!names.includes(INFLUXDB_DB)) {
+    return influx.createDatabase(INFLUXDB_DB)
+  }
+})
+.then(created => {
+  influx.createRetentionPolicy('90d', {
+    database: INFLUXDB_DB,
+    duration: '90d',
+    replication: 1,
+    isDefault: true
+  })
+})
+.catch(err => {
+  console.error(`Error creating Influx database!`)
+})
+
+let influxLogTick = (tick) => {
+  influx.writePoints([
+    {
+      measurement: 'btc_fee_tick',
+      fields: tick,
+      tags: { exchange: tick.exchange }
+    }
+  ]).catch(err => {
+    console.error(`Error saving data to InfluxDB! ${err.stack}`)
+  })
+}
+
+let influxLogFeeRec = (feeRecObj) => {
+  influx.writePoints([
+    {
+      measurement: 'btc_fee_rec',
+      fields: feeRecObj
+    }
+  ]).catch(err => {
+    console.error(`Error saving data to InfluxDB! ${err.stack}`)
+  })
+}
+
+// Exchange API request limits:
+//   bitfinex : 90/min : http://docs.bitfinex.com/docs
+//   bitstamp : 600/10min : https://www.bitstamp.net/api/
+let getExchangeTick = () => {
+  coinTicker('bitfinex', 'btcusd')
+  .then((tick) => {
+    // console.log('bitfinex exchange tick : %s', JSON.stringify(tick))
+    exchangeTick = tick
+    influxLogTick(tick)
   }).catch((err) => {
     console.error('bitfinex call failed, failing over to bitstamp : %s', JSON.stringify(err))
-    coinTicker('bitstamp', 'btcusd').then((data) => {
-      // console.log('bitstamp exchange data : %s', JSON.stringify(data))
-      currentExchange = data
+
+    coinTicker('bitstamp', 'btcusd').then((tick) => {
+      // console.log('bitstamp exchange tick : %s', JSON.stringify(tick))
+      exchangeTick = tick
+      influxLogTick(tick)
     }).catch((err) => {
       // Could not reach two exchanges. Something is very wrong.
       console.error('bitstamp call failed : %s', JSON.stringify(err))
@@ -56,8 +143,8 @@ let genFeeRecObj = (recFeeInSatoshiPerByte) => {
   obj.recFeeInBtcForAvgTx = feeBtcForAvgTx
 
   // Include USD estimation if available
-  if (_.has(currentExchange, 'last')) {
-    let fee = _.parseInt(currentExchange.last) * feeBtcForAvgTx
+  if (_.has(exchangeTick, 'last')) {
+    let fee = _.parseInt(exchangeTick.last) * feeBtcForAvgTx
     obj.recFeeInUsdForAvgTx = _.round(fee, 2)
   } else {
     obj.recFeeInUsdForAvgTx = null
@@ -67,8 +154,8 @@ let genFeeRecObj = (recFeeInSatoshiPerByte) => {
   return obj
 }
 
-// HTTP GET and cache current fees from
 let getRecommendedFees = () => {
+  // 21.co API Rate Limit : 5000/hour : https://bitcoinfees.21.co/api
   requestify.get(REC_FEES_URI, {
     cache: {
       cache: true,
@@ -87,7 +174,8 @@ let getRecommendedFees = () => {
       let selectedFee = _.parseInt(responseBody.fastestFee)
       let feeRecObj = genFeeRecObj(selectedFee)
 
-      console.log('fee recommendations %s', JSON.stringify(feeRecObj))
+      // console.log('fee recommendations %s', JSON.stringify(feeRecObj))
+      influxLogFeeRec(feeRecObj)
 
       // Publish the recommended transaction fee data in a well known
       // location for use by any service with access to Redis.
@@ -105,10 +193,10 @@ let getRecommendedFees = () => {
 }
 
 // get exchange rate at startup and periodically
-getCurrentExchange()
-setInterval(() => getCurrentExchange(), 1000 * 60 * 10) // 10 min
+getExchangeTick()
+setInterval(() => getExchangeTick(), 1000 * 60) // 1 min
 
 // get recommended fees at startup and periodically
-// most will retrieve from cache and not hit external API
+// most calls served from cache and don't hit external API
 getRecommendedFees()
-setInterval(() => getRecommendedFees(), 1000 * 5) // 5 seconds
+setInterval(() => getRecommendedFees(), 1000 * 60) // 1 min
