a359a87e47668c21d728f1b80a6aff336909cb6e
Jason Bukowski <jasonbukowski@users.noreply.github.com>
2017-07-09 01:14:16 -0400

    Added general functions to utils

--- node-lib/lib/utils.js
+++ node-lib/lib/utils.js
@@ -1,8 +1,49 @@
-// wait for a specified number of milliseconds to elapse
+/**
+ * Sleep for a specified number of milliseconds
+ *
+ * @param {number} ms - The number of milliseconds to sleep
+ */
 function sleep (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+/**
+ * Add specified minutes to a Date object
+ *
+ * @param {Date} date - The starting date
+ * @param {number} minutes - The number of minutes to add to the date
+ * @returns {Date}
+ */
+function addMinutes (date, minutes) {
+  return new Date(date.getTime() + (minutes * 60000))
+}
+
+/**
+ * Convert Date to ISO8601 string, stripping milliseconds
+ * '2017-03-19T23:24:32Z'
+ *
+ * @param {Date} date - The date to convert
+ * @returns {string} An ISO8601 formatted time string
+ */
+function formatDateISO8601NoMs (date) {
+  return date.toISOString().slice(0, 19) + 'Z'
+}
+
+/**
+ * Convert strings in an Array of hashes to lower case
+ *
+ * @param {string[]} hashes - An array of string hashes to convert to lower case
+ * @returns {string[]} An array of lowercase hash strings
+ */
+function lowerCaseHashes (hashes) {
+  return hashes.map((hash) => {
+    return hash.toLowerCase()
+  })
+}
+
 module.exports = {
-  sleep: sleep
+  sleep: sleep,
+  addMinutes: addMinutes,
+  formatDateISO8601NoMs: formatDateISO8601NoMs,
+  lowerCaseHashes: lowerCaseHashes
 }
