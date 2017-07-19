831a08d788cd428845705ecd10817a87eeb3ee7f
Jason Bukowski <jasonbukowski@users.noreply.github.com>
2017-07-19 19:54:46 -0400

    Fixed config error handling

--- node-api-service/lib/endpoints/config.js
+++ node-api-service/lib/endpoints/config.js
@@ -1,6 +1,7 @@
 const env = require('../parse-env.js')('api')
 
 const calendarBlock = require('../models/CalendarBlock.js')
+const restify = require('restify')
 
 // The redis connection used for all redis communication
 // This value is set once the connection has been established
@@ -21,31 +22,44 @@ function getCorePublicKeyList () {
  * Returns a configuration information object
  */
 async function getConfigInfoV1Async (req, res, next) {
-  let topCoreBlock = await CalendarBlock.findOne({ attributes: ['id'], order: [['id', 'DESC']] })
-  let latestChallengeKey = await redis.getAsync(`calendar_audit_challenge:latest_key`)
-  let latestChallenge = await redis.getAsync(latestChallengeKey)
-  // the challenge value contains the solution in the last segment, remove it before adding to teh response
-  let challengeSegments = latestChallenge.split(':')
-  challengeSegments.pop()
-  latestChallenge = challengeSegments.join(':')
+  let result
+  try {
+    let topCoreBlock = await CalendarBlock.findOne({ attributes: ['id'], order: [['id', 'DESC']] })
+    if (!topCoreBlock) throw new Error('no blocks found on calendar')
 
-  res.send({
-    chainpoint_base_uri: env.CHAINPOINT_BASE_URI,
-    anchor_btc: env.ANCHOR_BTC,
-    anchor_eth: env.ANCHOR_ETH,
-    proof_expire_minutes: env.PROOF_EXPIRE_MINUTES,
-    get_proofs_max_rest: env.GET_PROOFS_MAX_REST,
-    get_proofs_max_ws: env.GET_PROOFS_MAX_WS,
-    post_hashes_max: env.POST_HASHES_MAX,
-    post_verify_proofs_max: env.POST_VERIFY_PROOFS_MAX,
-    get_calendar_blocks_max: env.GET_CALENDAR_BLOCKS_MAX,
-    time: new Date().toISOString(),
-    public_keys: getCorePublicKeyList(),
-    calendar: {
-      height: parseInt(topCoreBlock.id),
-      audit_challenge: latestChallenge
+    let latestChallengeKey = await redis.getAsync(`calendar_audit_challenge:latest_key`)
+    let latestChallenge = await redis.getAsync(latestChallengeKey)
+
+    // the challenge value contains the solution in the last segment, remove it before adding to teh response
+    if (latestChallenge) {
+      let challengeSegments = latestChallenge.split(':')
+      challengeSegments.pop()
+      latestChallenge = challengeSegments.join(':')
     }
-  })
+
+    result = {
+      chainpoint_base_uri: env.CHAINPOINT_BASE_URI,
+      anchor_btc: env.ANCHOR_BTC,
+      anchor_eth: env.ANCHOR_ETH,
+      proof_expire_minutes: env.PROOF_EXPIRE_MINUTES,
+      get_proofs_max_rest: env.GET_PROOFS_MAX_REST,
+      get_proofs_max_ws: env.GET_PROOFS_MAX_WS,
+      post_hashes_max: env.POST_HASHES_MAX,
+      post_verify_proofs_max: env.POST_VERIFY_PROOFS_MAX,
+      get_calendar_blocks_max: env.GET_CALENDAR_BLOCKS_MAX,
+      time: new Date().toISOString(),
+      public_keys: getCorePublicKeyList(),
+      calendar: {
+        height: parseInt(topCoreBlock.id),
+        audit_challenge: latestChallenge || undefined
+      }
+    }
+  } catch (error) {
+    console.error(`could not generate config object : ${error}`)
+    return next(new restify.InternalServerError('server error'))
+  }
+
+  res.send(result)
   return next()
 }
 
