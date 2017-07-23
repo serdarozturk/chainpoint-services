26c60ec9656c22ddf46dcafcf4abe75ea2a746e5
pooleja <poole.james@gmail.com>
2017-07-23 15:16:02 -0700

    saving wip to server

--- node-eth-mon-service/loaders/providerLoader.js
+++ node-eth-mon-service/loaders/providerLoader.js
@@ -3,23 +3,26 @@ var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js')
 var Web3Subprovider = require('web3-provider-engine/subproviders/web3.js')
 var Web3 = require('web3')
 
-module.exports = (wallet) => {
+module.exports = (nodeUri, wallet) => {
   // Load the env var to figure out which node to connect to
-  let nodeUri = process.env.PROVIDER_URI
   if (!nodeUri) {
-    console.error('PROVIDER_URI environment variable not set.  Exiting...')
+    console.error('ETH_PROVIDER_URI environment variable not set.  Exiting...')
     process.exit(-1)
   }
 
   console.log('Using provider: ' + nodeUri)
 
+  // For development against testrpc, just use the default
+  if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging') {
+    return new Web3.providers.HttpProvider(nodeUri)
+  }
+
   var engine = new ProviderEngine()
   engine.addProvider(new WalletSubprovider(wallet, {}))
   engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(nodeUri)))
 
-  // Only start this if we are running in testnet - otherwise it holds the main thread open
-  if(process.env.NETWORK == 'roptsten'){
-    engine.start() // Required by the provider engine.
-  }
+  // Only start this if we are running against actual node - otherwise it holds the main thread open
+  engine.start() // Required by the provider engine.
+
   return engine
 }
--- node-eth-mon-service/loaders/tokenLoader.js
+++ node-eth-mon-service/loaders/tokenLoader.js
@@ -1,23 +1,18 @@
 var Web3 = require('web3')
-var loadWallet = require('./walletLoader.js')
-var loadProvider = require('./providerLoader.js')
 
 /*
  * This function will load the BCAP token ABI from the build directory and initialize
  * it so that it can be used to interact with the blockchain. It will use env variable
  * set for the address of the contract on the blockchain.
 */
-module.exports = () => {
-  // Read the wallet in from the file and create the provider
-  let wallet = loadWallet()
-  let provider = loadProvider(wallet)
+module.exports = (provider, tokenAddr) => {
+  // Create the provider
   let web3 = new Web3(provider)
 
-  // Set the default "from" account to the loaded wallet
-  web3.eth.defaultAccount = '0x' + wallet.getAddress().toString('hex')
+  // Set the default "from" account to the accounts
+  web3.eth.defaultAccount = web3.accounts[0]
 
   // First check to see the Token Address is set as an env var
-  let tokenAddr = process.env.BCAP_TOKEN_ADDR
   if (!tokenAddr) {
     console.error('BCAP Token ERC20 Contract Address is not set as env var (BCAP_TOKEN_ADDR).  Exiting...')
     process.exit(-1)
