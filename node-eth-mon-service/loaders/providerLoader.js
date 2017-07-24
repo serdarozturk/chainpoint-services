1d671e0810a30674b6f6bc639598096d81989b4c
pooleja <poole.james@gmail.com>
2017-07-23 17:41:57 -0700

    Cleaned up wallet provider to rely on node wallet

--- node-eth-mon-service/loaders/providerLoader.js
+++ node-eth-mon-service/loaders/providerLoader.js
@@ -1,6 +1,3 @@
-var ProviderEngine = require('web3-provider-engine')
-var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js')
-var Web3Subprovider = require('web3-provider-engine/subproviders/web3.js')
 var Web3 = require('web3')
 
 module.exports = (nodeUri, wallet) => {
@@ -11,18 +8,5 @@ module.exports = (nodeUri, wallet) => {
   }
 
   console.log('Using provider: ' + nodeUri)
-
-  // For development against testrpc, just use the default
-  if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging') {
-    return new Web3.providers.HttpProvider(nodeUri)
-  }
-
-  var engine = new ProviderEngine()
-  engine.addProvider(new WalletSubprovider(wallet, {}))
-  engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(nodeUri)))
-
-  // Only start this if we are running against actual node - otherwise it holds the main thread open
-  engine.start() // Required by the provider engine.
-
-  return engine
+  return new Web3.providers.HttpProvider(nodeUri)
 }
--- node-eth-mon-service/truffle.js
+++ node-eth-mon-service/truffle.js
@@ -1,11 +1,4 @@
 require('dotenv').config()
-var loadWallet = require('./loaders/walletLoader.js')
-var loadProvider = require('./loaders/providerLoader.js')
-
-// Read the wallet in from the file
-let wallet = loadWallet()
-var address = '0x' + wallet.getAddress().toString('hex')
-console.log('Using this address for source transactions on Ropsten: ' + address)
 
 /**
  * This export is used by various truffle scripts to determine how to connect to different ETH networks.
@@ -17,12 +10,6 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
-    },
-    ropsten: {
-      network_id: 3,                   // Ropsten network id
-      provider: loadProvider(wallet),  // Use our custom provider
-      from: address,                   // Use the source address we derived to sign transactions.
-      gas: 4000000                     // Ran into some bugs related to gas... this seemed to fix it.
     }
   }
 }
