b76e59603ff1d7d664b104ec9283b0e3069235dd
pooleja <poole.james@gmail.com>
2017-07-25 11:05:19 -0700

    Update loader to try and use def

--- node-eth-mon-service/loaders/tokenLoader.js
+++ node-eth-mon-service/loaders/tokenLoader.js
@@ -1,29 +1,43 @@
 var Web3 = require('web3')
-
+const contract = require('truffle-contract')
+const TokenContract = require('../build/contracts/BCAPTestToken.json')
 /*
  * This function will load the BCAP token ABI from the build directory and initialize
  * it so that it can be used to interact with the blockchain. It will use env variable
  * set for the address of the contract on the blockchain.
 */
-module.exports = (provider, tokenAddr) => {
+module.exports = async (provider, tokenAddr) => {
   // Create the provider
   let web3 = new Web3(provider)
 
   // Set the default "from" account to the accounts
   web3.eth.defaultAccount = web3.eth.accounts[0]
 
-  // First check to see the Token Address is set as an env var
-  if (!tokenAddr) {
-    console.error('BCAP Token ERC20 Contract Address is not set as env var (BCAP_TOKEN_ADDR).  Exiting...')
+  // Load the token json obj
+  let tokenDef = require('../build/contracts/BCAPTestToken.json')
+
+  // If the token addr is specified in the environment var, use that as highest priority
+  if (tokenAddr) {
+    console.log('Using token addr: ' + tokenAddr)
+
+    // Load the ABI for the contract and initialize a contract interface
+    let tokenABI = tokenDef.abi
+    let tokenDefinition = web3.eth.contract(tokenABI)
+
+    // Set the actual instance from the address on the blockchain, so we can communicate with it.
+    return tokenDefinition.at(tokenAddr)
+  }
+
+  // If the env var was not set, see if the token definition has been deployed.
+  const token = contract(TokenContract)
+  let deployedToken = await token.deployed()
+
+  // Didn't find it there either... bail out
+  if (!deployedToken) {
+    console.error('BCAP Token ERC20 Contract Address is not found deployed or set as env var (BCAP_TOKEN_ADDR).  Exiting...')
     process.exit(-1)
   }
 
-  console.log('Using token addr: ' + tokenAddr)
-
-  // Load the ABI for the contract and initialize a contract interface
-  let tokenABI = require('../build/contracts/BCAPTestToken.json').abi
-  let tokenDefinition = web3.eth.contract(tokenABI)
-
-  // Set the actual instance from the address on the blockchain, so we can communicate with it.
-  return tokenDefinition.at(tokenAddr)
+  // Return the deployed instance
+  return deployedToken
 }
